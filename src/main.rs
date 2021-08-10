99aeec2f1349342a25e830f73987fbd575c9ac00
Justus Croskery <justus@olmmcc.tk>
2021-08-10 10:50:13 -0400

    Add simple get response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, large world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+const PORT: i32 = 8080;
+const MESSAGE: &str = "Hello world!";
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body(MESSAGE)
 }
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(hello)
+    })
+    .bind(format!("127.0.0.1:{}", PORT))?
+    .run()
+    .await
+}
+
